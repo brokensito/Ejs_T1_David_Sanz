ba8854d474d8b104510147f889c954d2ed320caa
brokensito <dsanzfuertes.15@gmail.com>
2022-09-26 19:36:56 +0200

    Resolucion ej1

---
+++ ej1.py
@@ -0,0 +1,11 @@
+cadena = "zeréP nauJ,01"
+
+# Invertimos la cadena
+
+cadena_final = cadena[::-1].split(",")
+
+# Imprimimos el resultado de la cadena inversa
+
+print(cadena_final[1],"ha sacdo un", cadena_final[0], "de nota")
+
+
