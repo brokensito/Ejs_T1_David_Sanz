737cee496a6539e213d1bf7056f2e721e7e6f6b8
brokensito <dsanzfuertes.15@gmail.com>
2022-09-27 16:05:41 +0200

    Ej 3 comentario
    Comentarios agregados acerca de la resolucion del ejercicio

--- ej3.py
+++ ej3.py
@@ -7,12 +7,17 @@ lista_2 = ["h",'o','l','a',' ', 'l','u','n','a']
 
 lista_3 = []
 
-for i, character in enumerate(lista_1):
+# Creamos una lista donde agregamos los valores repetidos de las dos listas
 
+
+for i, character in enumerate(lista_1):
     if lista_1[i] in lista_2:
         lista_3.append(character)
 
 lista_3 = list(set(lista_3))
+
+# Utilizamos la funcion set para convertir la lista en un conjunto y eliminar los elementos 
+# duplicados dentro de la lista. Luego convertimos ese mismo conjunto en lista y lo imprimimos.
 
 print(lista_3)
 
