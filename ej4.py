e70068139d55b981dc1be4dcf7a089958bee004f
brokensito <dsanzfuertes.15@gmail.com>
2022-09-28 21:42:24 +0200

    Ej4 finalizado
    Finalmente creamos la estructura de tipo cola y la iteramos durante el largo de la cola, imprimiendo unicamente el nombre de la lista

--- ej4.py
+++ ej4.py
@@ -1,12 +1,16 @@
 from collections import deque
 
+# Primero creamos una serie de actividades y las ordenamos de menor a mayor utilizando sort()
 
 actividades = [[1, "Ducharse"],[9, "Fiesta"],[5, "Estudiar"], [8, "Beber"]]
-
 actividades.sort()
 
-lista_final = deque()
+# Creamos una estructura de tipo cola donde metemos las actividades ya ordenadas
 
+lista_final = deque(actividades)
 
+# Iteramos el largo de la cola e imprimimos unicamente el nombre de la tarea
 
+for i in range(len(lista_final)):
+    print(lista_final[i][1])
 
